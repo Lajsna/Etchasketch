55faebe5a60f39fd337109b28a5e7c584521d92f
Nikola Miskovic <miskovicnikola0@gmail.com>
2015-11-29 03:35:25 +0100

    Finished

--- js/The-Magic.js
+++ js/The-Magic.js
@@ -36,34 +36,50 @@ $(document).ready(function(){
 var gridInit=function(gsize){
 	$('#grid').empty();
 	for(i=1;i<=gsize*gsize;i++)
-	$('#grid').append($("<div class='piece'></div"));
+	$('#grid').append($("<div class='piece' data-red='0' data-green='0' data-blue='0' data-i='0'></div"));
 	$('.piece').height(1200/gsize);
 	$('.piece').width(1200/gsize);
 }
 var funk=function(int){
+	$('.piece').data('i',0);
+	$('.piece').unbind("mouseenter mouseleave");
 	switch(int){
 		case 1:
-			$('.piece').mouseenter(function(){
+			$('.piece').bind("mouseenter",function(){
 				$(this).css("background-color","white");
 		});
 		break;
 		case 2:
-			$('.piece').mouseenter(function(){
-				var randC ="rgb("+ Math.floor(Math.random()*254 +1) + "," + Math.floor(Math.random()*254 +1) + "," + Math.floor(Math.random()*254 +1)+")";
-				$(this).css("background-color",randC);
+			$('.piece').bind("mouseenter",function(){
+				$(this).css("background-color","rgb("+Math.floor(Math.random()*254+1)+","+Math.floor(Math.random()*254+1)+","+Math.floor(Math.random()*254+1)+")");
 		});
 		break;
 		case 3:
-			$('.piece').mouseenter(function(){
-				alert($(this).css('background-color'));
-		});
+			$('.piece').bind("mouseenter",function(){
+				if($(this).data("i")===0){
+					$(this).data("red", Math.floor(Math.random()*254 +1));
+					$(this).data("green", Math.floor(Math.random()*254 +1));
+					$(this).data("blue", Math.floor(Math.random()*254 +1));
+					$(this).css("background-color","rgb("+$(this).data('red')+","+$(this).data('green')+","+$(this).data('blue')+")");
+					$(this).data("i",1);
+				}
+				else if($(this).data('i')<=10){
+					var i=$(this).data('i');
+					var pcrvena = Math.floor($(this).data('red')/10 * i);
+					var pzelena = Math.floor($(this).data('green')/10 * i);
+					var pplava = Math.floor($(this).data('blue')/10 * i);
+					i++;
+					$(this).css("background-color","rgb("+($(this).data('red')-pcrvena)+","+($(this).data('green')-pzelena)+","+($(this).data('blue')-pplava)+")");
+					$(this).data('i',i);
+				}
+			});
 		break;
 		case 4:
-			$('.piece').mouseenter(function(){
+			$('.piece').bind("mouseenter",function(){
 				$(this).fadeTo(0,0);
 				$(this).css("background-color","white");
 			});
-			$('.piece').mouseleave(function(){
+			$('.piece').bind("mouseleave",function(){
 				$(this).fadeTo('slow',1);
 				$(this).css("background-color","green");
 			});
